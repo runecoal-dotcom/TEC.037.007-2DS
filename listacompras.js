3669601c86628650cf22ef4366fe3ddad3308608
andersoncarvalhos19 <milaqc.bispo@gmail.com>
2026-03-12 12:17:53 -0300

    lista-nome

---
+++ listacompras.js
@@ -0,0 +1,13 @@
+let lista_compras = ["Arroz", "Feijão", "Macarrão", "Açúcar", "Salmão"]
+lista_compras.push("Carne")
+lista_compras.push("Frango")
+lista_compras.push("Leite")
+lista_compras.push("Desinfetante")
+lista_compras.forEach(item => {
+    console.log(item)
+})
+
+let cesta_frutas = ["Banana", "Maçã", "Laranja", "Abacaxi", "Uva"]
+cesta_frutas.forEach(fruta => {
+    console.log(fruta)
+})
