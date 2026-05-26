cb330201c1a2bebb002c53c01ea9aaf1396c9943
Rune <milaqc.bispo@gmail.com>
2026-05-26 12:17:03 -0300

    RPG 4

--- atividade_RPG/javascript.js
+++ atividade_RPG/javascript.js
@@ -1,14 +1,41 @@
 class Personagem {
-    constructor(nome, titulo, hp, mana,energia) {
+    constructor(nome, titulo, hp, mana, energia) {
         this.nome = nome;
         this.titulo = titulo;
         this.hp = hp;
         this.mana = mana;
         this.energia = energia;
     }
+    hero_atacar(alvo, habilidade) {
+        if (this.mana >= habilidade.custo
+            && this.energia >= habilidade.energia) {
+            alvo.hp = alvo.hp - habilidade.dano;
+            
+            if (habilidade.custo > 0) {
+                this.mana -= habilidade.custo;
+                this.energia += 50;
+                
+            }
+            if (habilidade.energia > 0) {
+                this.energia = 0;
+            }
+            
+            else {
+                return ("Mana ou Energia Insuficiente!");
+            }
+        }
+    }
+    boss_atacar(alvo) {
+        if (this.energia >= 100) {
+            alvo.hp = alvo.hp - 15;
+            this.energia = 0;
+        } else {
+            this.energia += 50;
+        }
+    }
 }
 class Habilidade {
-    constructor(id,nome, dano, custo, energia) {
+    constructor(id, nome, dano, custo, energia) {
         this.id = id;
         this.nome = nome;
         this.dano = dano;
@@ -24,23 +51,35 @@ let hero2 = new Personagem("Aurora", "Hera Venenosa", 100, 0, 50);
 
 document.getElementById("nome-hero").textContent = hero.nome;
 document.getElementById("titulo-hero").textContent = hero.titulo;
-document.getElementById("hp-hero").value = hero.hp;
-document.getElementById("mana-hero").value = hero.mana;
-document.getElementById("energia-hero").value = hero.energia;
 
 document.getElementById("nome-hero2").textContent = hero2.nome;
 document.getElementById("titulo-hero2").textContent = hero2.titulo;
-document.getElementById("hp-hero2").value = hero2.hp;
-document.getElementById("mana-hero2").value = hero2.mana;
-document.getElementById("energia-hero2").value = hero2.energia;
 
+let containerBtn = document.getElementById("controles");
 let ListaHabilidades = [
-    new Habilidade(1, "🏹 Ataque Básico", 4, 0, 0),
-    new Habilidade(2, "🪄 Skill", 8, 10, 0),
-    new Habilidade(3, "🧨 Supremo", 15, 0, 100)
+    new Habilidade(1, "🏹Ataque Básico", 4, 0, 0),
+    new Habilidade(2, "🪄Skill", 8, 10, 0),
+    new Habilidade(3, "🧨Supremo", 15, 0, 100)
 ];
+console.log("ListaHabilidades");
 ListaHabilidades.forEach(hab => {
+    console.log(hab);
     let btn = document.createElement("button");
     btn.innerText = hab.nome;
+    btn.classList.add("btn", "btn-primary");
     containerBtn.appendChild(btn);
+    btn.onclick = () => {
+        hero.hero_atacar(hero2, hab);
+        document.getElementById("hp-hero2").value = hero2.hp;
+        atualizartela()
+    }
 })
+const atualizartela = () => {
+    document.getElementById("hp-hero2").value = hero2.hp;
+    document.getElementById("mana-hero2").value = hero2.mana;
+    document.getElementById("energia-hero2").value = hero2.energia;
+
+    document.getElementById("hp-hero").value = hero.hp;
+    document.getElementById("mana-hero").value = hero.mana;
+    document.getElementById("energia-hero").value = hero.energia;
+}
